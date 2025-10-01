d7c1d383574d7ed8a3608c7fe549e8edfe0f64d5
Arun Kumar <pheron408@gmail.com>
2025-10-01 19:31:50 +0530

    feat/redis_config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,34 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { WorkspaceModule } from './workspaces/workspaces.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, WorkspaceModule],
+  imports: [
+    //load configuration from .env files golbally..
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    //2. stepup caching using Redis Store..
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: (ConfigService: ConfigService) => ({
+        store: redisStore,
+        host: ConfigService.get<string>('redis_Host') || 'localhost',
+        port: ConfigService.get<number>('redis_Port') || 6379,
+        ttl: 1000 * 60 * 5,
+      }),
+      inject: [ConfigService],
+    }),
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    WorkspaceModule,
+  ],
   controllers: [],
   providers: [],
 })
