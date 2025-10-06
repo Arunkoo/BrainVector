2fc8b784e2ea82b833fbf818a0246b08fdecd7f5
Arun Kumar <pheron408@gmail.com>
2025-10-06 22:42:57 +0530

    update/documentModule

--- backend/src/documents/document.module.ts
+++ backend/src/documents/document.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { DocumentService } from './document.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [DocumentService],
+  exports: [DocumentService],
 })
 export class DocumentModule {}
