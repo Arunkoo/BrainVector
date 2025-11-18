6469fdc2f12354c0aeb27437d9de5152adf29516
Arun Kumar <pheron408@gmail.com>
2025-11-18 21:36:31 +0530

    Linked api using axios with backend but not tested

---
+++ Client/src/api/auth.api.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import type { LoginDto, RegisterDto, User } from "../types";
+
+const api = axios.create({
+  baseURL: import.meta.env.Vite_API_URL || "http://localhost:3000/auth",
+  withCredentials: true,
+});
+
+export const authApi = {
+  checkAuth: async (): Promise<User> => {
+    const response = await api.get<User>("/me");
+    return response.data;
+  },
+
+  login: async (credentials: LoginDto): Promise<User> => {
+    const response = await api.post<User>("/login", credentials);
+
+    return response.data;
+  },
+
+  register: async (userData: RegisterDto): Promise<User> => {
+    const response = await api.post<User>("/register", userData);
+    return response.data;
+  },
+
+  logout: async (): Promise<{ message: string }> => {
+    const response = await api.post("/logout");
+    return response.data;
+  },
+};
