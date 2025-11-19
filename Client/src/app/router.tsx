beb1c33afbbdb4761a93842c1902bc218f2adf24
Arun Kumar <pheron408@gmail.com>
2025-11-19 22:02:26 +0530

    login form written

--- Client/src/app/router.tsx
+++ Client/src/app/router.tsx
@@ -1,17 +1,38 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import AppLayout from "../components/Layout/AppLayout";
+import LoginPage from "../pages/LoginPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <AppLayout />,
-  },
-  {
-    path: "/login",
-  },
-  {
-    path: "*",
+    element: <AppLayout />, // AppLayout is the parent wrapper
+    children: [
+      // {
+      //   // index: true, // This makes it the default content for the root path ("/")
+      //   // element: <HomePage />,
+      // },
+      // 2. Login Page (Public)
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      // 3. Dashboard (Protected)
+      {
+        path: "dashboard",
+        // Wrap the protected content with the <ProtectedRoute> component
+        // element: (
+        //   <ProtectedRoute>
+        //     <DashboardPage />
+        //   </ProtectedRoute>
+        // ),
+      },
+      // 4. Catch-all path for 404
+      {
+        path: "*",
+        element: <div>404 Not Found</div>,
+      },
+    ],
   },
 ]);
 
